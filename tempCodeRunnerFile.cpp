8138427cb0201eca54bbdbe1ad57e11d6972e1d4
Janitha Rathnayaka <141403556+JanithaB@users.noreply.github.com>
2025-01-28 17:55:00 +0530

    added new files

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n){
+    if(n<=1){
+        return n;
+    }
+    else{
+        return (fib(n-1)+fib(n-2));
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter a val: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<fib(i)<<" ";
+    }
+
+    return 0;
+}
